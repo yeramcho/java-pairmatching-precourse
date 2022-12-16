0069cbff6f3db90b7ef1057c208657fc21e06cb1
choyeram <choyeram8@gmail.com>
2022-12-16 12:25:20 +0900

    refactor: 컨트롤러 구조 변경

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,10 +1,12 @@
 package pairmatching;
 
 import pairmatching.controller.PairMatchingController;
+import pairmatching.model.PairMatcher;
 
 public class Application {
     public static void main(String[] args) {
-        PairMatchingController pairMatchingController = new PairMatchingController();
+        PairMatcher pairMatcher = new PairMatcher();
+        PairMatchingController pairMatchingController = new PairMatchingController(pairMatcher);
         pairMatchingController.run();
     }
 }
--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,8 +1,16 @@
 package pairmatching.controller;
 
+import pairmatching.model.PairMatcher;
+import pairmatching.view.OutputView;
+
 public class PairMatchingController {
+    private final PairMatcher pairMatcher;
+
+    public PairMatchingController(PairMatcher pairMatcher) {
+        this.pairMatcher = pairMatcher;
+    }
 
     public void run() {
-
+        OutputView.selectFunction();
     }
 }
