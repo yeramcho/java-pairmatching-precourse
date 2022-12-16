27142743022c51a118c0a67e2df097c7aa0d6fbc
choyeram <choyeram8@gmail.com>
2022-12-16 12:17:20 +0900

    feat: 기능 선택 요구 메시지 출력

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,9 @@
+package pairmatching.view;
+
+public class OutputView {
+
+    public static void selectFunction() {
+        System.out.println(ProgramMessage.SELECT_FUNCTION.getMessage());
+        System.out.println(ProgramMessage.OPTIONS.getMessage());
+    }
+}
---
+++ src/main/java/pairmatching/view/ProgramMessage.java
@@ -0,0 +1,17 @@
+package pairmatching.view;
+
+public enum ProgramMessage {
+    SELECT_FUNCTION("기능을 선택하세요."),
+    OPTIONS("1. 페어 매칭\n" +
+            "2. 페어 조회\n" +
+            "3. 페어 초기화\n" +
+            "Q. 종료\n");
+
+    private final String message;
+    ProgramMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+}
